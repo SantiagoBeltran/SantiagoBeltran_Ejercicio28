4754acdf9e7fb12d4e8f2b40a0839b4c13b18912
SantiagoBeltran <46940845+SantiagoBeltran@users.noreply.github.com>
2019-05-01 12:09:26 -0500

    Ecuacion de calor

--- SantiagoBeltran_Ejercicio28.cpp
+++ SantiagoBeltran_Ejercicio28.cpp
@@ -3,53 +3,58 @@
 #include <stdlib.h>
 using namespace std;
 
-void laplaceline(int iter, int nmax, string filename);
+void laplaceline(int nmax, int nt,string filename);
 
 int main(){
-  laplaceline(10000, 101, "intento.dat");
+  laplaceline(100, 4000, "intento.dat");
   return 0;
 }
 
-void laplaceline(int iter, int nmax,string filename){
-    float V_old[nmax][nmax]={0};
-    float V_new[nmax][nmax]={0};
+void laplaceline(int nmax, int nt,string filename){
+    float T_old[nmax][nt]={0};
+    float T_new[nmax][nt]={0};
+    const float dx=0.01;
+    const float dt=0.01;
+    const float K=237;
+    const float C=900;
+    const float r=2700;
+    float n;
     ofstream outfile;
   outfile.open(filename);
-    for(int i=20;i<=80;i++){
-        V_old[i][40]=-100;V_new[i][40]=-100;
-        V_old[i][60]=100;V_new[i][60]=100;
-        }
+    for (int i=0; i<nmax;i++){
+        T_old[i][0]=100;T_new[i][0]=100;}
     
+    n=K*dt/(C*r*dx*dx);
     float error=100.0;
     while(error>0.00001){
     
         for(int i=1; i<nmax-1; i++){
-        for(int j=1; j<nmax-1; j++){
-            if(!((20<=i&& i<=80 && j==40)||(20<=i&& i<=80 && j==60))){
+        for(int j=0; j<nt-1; j++){
+            if(i!=0&&i!=nmax-1){
             
-            V_new[i][j]=0.25*(V_old[i+1][j]+V_old[i-1][j]+V_old[i][j+1]+V_old[i][j-1]);}
+            T_new[i][j+1]=T_old[i][j]+n*(T_old[i+1][j]+T_old[i-1][j]-2*T_old[i][j]);}
         }
         }
         int cont=0;  
         error=0.0;    
         for(int i=1; i<nmax-1; i++){
-        for(int j=1; j<nmax-1; j++){
-            error=error+abs(V_new[i][j]-V_old[i][j]);
+        for(int j=0; j<nt-1; j++){
+            error=error+abs(T_new[i][j]-T_old[i][j]);
             cont=cont+1;
         }    
     }
         error=error/cont;
     for(int i=0; i<nmax; i++){
-        for(int j=0; j<nmax; j++){
-            V_old[i][j]=V_new[i][j];
+        for(int j=0; j<nt; j++){
+            T_old[i][j]=T_new[i][j];
         }       
 }    
     }
     
     for(int i=0; i<nmax; i++){
-        for(int j=0; j<nmax; j++){
-            outfile << V_old[i][j] << " ";
-            if(j==nmax-1){outfile<<endl;}
+        for(int j=0; j<nt; j++){
+            outfile << T_new[i][j] << " ";
+            if(j==nt-1){outfile<<endl;}
         } 
 }
 }

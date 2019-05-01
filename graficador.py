7d1aed1b643006a0cb52c6e80f21ed2cfb008aa7
SantiagoBeltran <46940845+SantiagoBeltran@users.noreply.github.com>
2019-05-01 12:08:41 -0500

    Graficar calor

--- graficador.py
+++ graficador.py
@@ -4,9 +4,9 @@ from mpl_toolkits.mplot3d import Axes3D
 
 data= np.loadtxt("intento.dat")
 x=np.arange(0,100,1)
-y=np.arange(0,100,1)
-x1=np.arange(0,100,1)/100
-y1=np.arange(0,100,1)/100
+y=np.arange(0,4000,1)
+x1=np.arange(0,100,1)*0.01
+y1=np.arange(0,4000,1)*0.01
 
 def f(x,y):
     return data[x,y]
@@ -18,4 +18,4 @@ fig = plt.figure()
 ax = Axes3D(fig)
 ax.plot_wireframe(X1,Y1,f(X,Y),color="r")
 plt.show()
-plt.savefig("laplace.png")
+plt.savefig("calor.png")

6eacbcdcce9559ae3d90cfbdd2d6a148099fa0cd
SantiagoBeltran <46940845+SantiagoBeltran@users.noreply.github.com>
2019-05-01 11:23:30 -0500

    Cambio limites

--- graficador.py
+++ graficador.py
@@ -3,16 +3,19 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 data= np.loadtxt("intento.dat")
-x=np.arange(0,70,1)
-y=np.arange(0,70,1)
+x=np.arange(0,100,1)
+y=np.arange(0,100,1)
+x1=np.arange(0,100,1)/100
+y1=np.arange(0,100,1)/100
 
 def f(x,y):
     return data[x,y]
 
 X,Y=np.meshgrid(x,y)
+X1,Y1=np.meshgrid(x1,y1)
 
 fig = plt.figure()
 ax = Axes3D(fig)
-ax.plot_wireframe(X,Y,f(X,Y),color="r")
+ax.plot_wireframe(X1,Y1,f(X,Y),color="r")
 plt.show()
 plt.savefig("laplace.png")

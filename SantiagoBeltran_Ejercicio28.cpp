f7c7b70fd250ec4c525cb9135b4d7d6c0c8a97c6
SantiagoBeltran <ss.beltran@uniandes.eduy.co>
2019-05-02 12:29:09 +0000

    Solución ecuación del calor

--- SantiagoBeltran_Ejercicio28.cpp
+++ SantiagoBeltran_Ejercicio28.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void laplaceline(int nmax, int nt,string filename);
 
 int main(){
-  laplaceline(100, 4000, "intento.dat");
+  laplaceline(201, 201, "intento.dat");
   return 0;
 }
 
@@ -14,15 +14,19 @@ void laplaceline(int nmax, int nt,string filename){
     float T_old[nmax][nt]={0};
     float T_new[nmax][nt]={0};
     const float dx=0.01;
-    const float dt=0.01;
-    const float K=237;
+    const float dt=0.5;
+    const float K=200;
     const float C=900;
     const float r=2700;
     float n;
     ofstream outfile;
   outfile.open(filename);
     for (int i=0; i<nmax;i++){
-        T_old[i][0]=100;T_new[i][0]=100;}
+        T_old[i][0]=300;T_new[i][0]=300;
+        if(i>=80&&i<=120){
+        T_old[i][0]=500;T_new[i][0]=500;}}
+    for (int j=0; j<nt;j++){
+        T_old[0][j]=300;T_old[nmax-1][j]=300;T_new[0][j]=300;T_new[nmax-1][j]=300;}
     
     n=K*dt/(C*r*dx*dx);
     float error=100.0;
@@ -35,15 +39,13 @@ void laplaceline(int nmax, int nt,string filename){
             T_new[i][j+1]=T_old[i][j]+n*(T_old[i+1][j]+T_old[i-1][j]-2*T_old[i][j]);}
         }
         }
-        int cont=0;  
+          
         error=0.0;    
         for(int i=1; i<nmax-1; i++){
         for(int j=0; j<nt-1; j++){
             error=error+abs(T_new[i][j]-T_old[i][j]);
-            cont=cont+1;
         }    
     }
-        error=error/cont;
     for(int i=0; i<nmax; i++){
         for(int j=0; j<nt; j++){
             T_old[i][j]=T_new[i][j];

785cb2c7c99b4332004bb611e2c559d0808bfbb1
SantiagoBeltran <ss.beltran@uniandes.eduy.co>
2019-05-02 12:29:28 +0000

    Graficador

--- graficador.py
+++ graficador.py
@@ -1,21 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
-data= np.loadtxt("intento.dat")
-x=np.arange(0,100,1)
-y=np.arange(0,4000,1)
-x1=np.arange(0,100,1)*0.01
-y1=np.arange(0,4000,1)*0.01
+data = np.loadtxt("intento.dat")
+n_points = int(np.sqrt(len(data)))
+grid = np.reshape(data.T, (201, 201))
 
-def f(x,y):
-    return data[x,y]
 
-X,Y=np.meshgrid(x,y)
-X1,Y1=np.meshgrid(x1,y1)
+plt.figure(figsize=(15,5))
+plt.subplot(1,2,1)
+plt.imshow(grid)
+plt.xlabel("Indice X")
+plt.ylabel("Indice T")
+plt.colorbar(label="Temperatura")
 
-fig = plt.figure()
-ax = Axes3D(fig)
-ax.plot_wireframe(X1,Y1,f(X,Y),color="r")
-plt.show()
+
+T1=data[:,0]
+T2=data[:,-1]
+plt.subplot(1,2,2)
+x=np.arange(0,201,1)
+plt.xlabel("Indice X")
+plt.ylabel("Temperatura")
+plt.plot(x,T1, label="tiempo inicial")
+plt.plot(x,T2, label="tiempo final")
+plt.legend()
 plt.savefig("calor.png")
